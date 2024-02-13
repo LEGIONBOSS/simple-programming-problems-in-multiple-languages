613eee612d051001b2cec6ee0783eff8e15d5554
Szilágyi Dániel <szilagyi.daniel88@gmail.com>
2024-02-13 11:56:21 +0100

    typo fix

--- C/1 Elementary/1.5 Sum of 1 to n, only multiples of 3 and 5/main.c
+++ C/1 Elementary/1.5 Sum of 1 to n, only multiples of 3 and 5/main.c
@@ -1,6 +1,6 @@
 /*
 Modify the previous program such that only multiples of three or five are
-considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17.
 */
 
 #include <stdio.h>
