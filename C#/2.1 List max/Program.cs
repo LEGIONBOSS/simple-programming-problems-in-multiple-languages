19888b26e829e593e9216bb092fe42da85ee86bf
Dániel Szilágyi <szilagyi.daniel88@gmail.com>
2024-11-06 10:41:59 +0100

    C# 2.1

---
+++ C#/2.1 List max/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Write a function that returns the largest element in a list.
+*/
+
+using Generator;
+
+namespace _2._1_List_max
+{
+    internal class Program
+    {
+        private static int GetLargest(List<int> list) => list.Max();
+
+        static void Main(string[] args)
+        {
+            List<int> list = ListGen.GetList(100);
+
+            // List elements
+            Console.Write("Elements: ");
+            list.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+
+            // Print largest element
+            Console.WriteLine("Largest element: " + GetLargest(list));
+        }
+    }
+}
---
+++ C#/Generator/Generator.cs
@@ -0,0 +1,33 @@
+﻿namespace Generator
+{
+    public static class ListGen
+    {
+        private static Random _Rand = new Random();
+
+        public static int[] GetArray(uint size, int min = 0, int max = 100)
+        {
+            if (size == 0) size = 1;
+            if (max < min) max = min;
+
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = _Rand.Next(min, max + 1);
+            }
+            return array;
+        }
+
+        public static List<int> GetList(uint size, int min = 0, int max = 100)
+        {
+            if (size == 0) size = 1;
+            if (max < min) max = min;
+
+            List<int> list = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(_Rand.Next(min, max + 1));
+            }
+            return list;
+        }
+    }
+}
