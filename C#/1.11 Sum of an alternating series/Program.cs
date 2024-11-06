079d715d5aeac40133d4ae3261780f159b60536f
Dániel Szilágyi <szilagyi.daniel88@gmail.com>
2024-11-06 10:06:13 +0100

    C# 1.11

---
+++ C#/1.11 Sum of an alternating series/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Write a program that computes the sum of an alternating series where each element of the series is an expression of the form ((-1)^k+1)/(2*k-1) for each value of k from 1 to a million, multiplied by 4.
+*/
+
+/* Hint: This is a PI approximation*/
+
+namespace _1._11_Sum_of_an_alternating_series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            for (int k = 1; k <= 1000000; k++)
+            {
+                sum += Math.Pow(-1, k + 1) / (2 * k - 1);
+            }
+
+            double result = 4 * sum;
+
+            Console.WriteLine(result);
+        }
+    }
+}
